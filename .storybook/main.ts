6351d811e4a0c48a143419c50c024a2350b93086
GuoFei <guofei0723@gmail.com>
2023-05-10 23:11:34 +0800

    build: init storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,33 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  webpackFinal(config, options) {
+    if (!config.resolve) {
+      config.resolve = {
+        plugins: [],
+      };
+    }
+
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin(),
+    ];
+
+    return config;
+  },
+};
+export default config;
